001ff7afcf243759eba7ce2a374af23679f206d5
Jacky Cao <jcvan0116@gmail.com>
2021-11-19 10:20:01 -0800

    header WIP

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,19 @@
 import logo from './logo.svg';
 import './App.scss';
 import Images from './components/Images/Images';
+import Header from './components/Header/Header';
 // import HomePage from "./pages/HomePage/HomePage"
 // import HomePage from "./pages/HomePage/HomePage"
+import DrawAnnotations from './components/DrawAnnotations/DrawAnnotations';
 import DrawRectangle from "./pages/DrawRectangle/DrawRectangle"
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 const App = () => {
   return (
     <div className="App">
-      <Images/>
+      <Header/>
+      {/* <Images />
+      <DrawAnnotations/> */}
     </div>
   );
     // <BrowserRouter>
---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,25 @@
+
+import { Component } from "react";
+import "./Header.scss"
+
+
+class Header extends Component {
+    render() {
+        return (
+            <div className="header">
+                <span className="header__logo header__component">logo</span>
+                <span className="header__title header__component">GM rewards</span>
+                <span className="header__burger-menu header__component">
+                    <svg viewBox="0 0 100 80" width="40" height="40">
+                        <rect width="100" height="20"></rect>
+                        <rect y="30" width="100" height="10"></rect>
+                        <rect y="60" width="100" height="10"></rect>
+                    </svg>
+                </span>
+
+            </div>
+        );
+    }
+}
+
+export default Header;
