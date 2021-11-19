0749382e1903cf5dfec17fa93d2bae24f4c1fff4
Jacky Cao <jcvan0116@gmail.com>
2021-11-19 00:01:44 -0800

    image page set up

---
+++ client/src/component/Images/Images.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import "./Images.scss"
+import axios from "axios"
+// import { Link } from 'react-router-dom'
+
+export default class Images extends Component {
+    state = {
+        imagesId: [0,1,2,3,4],
+        squareDetail: [], 
+        imagesIndex: 4
+    };
+
+
+
+
+    render(){
+        const nextImage = () => {
+            this.setState({
+                imagesIndex: this.state.imagesIndex + 1
+            });
+        }
+        return(
+            <>
+                <button type="submit" onClick={this.nextImage}>Next</button>
+                <img src={`http://localhost:8080/image${this.state.imagesId[this.state.imagesIndex]}.jpeg`} />
+                {/* <Link to="/rewards">Submit</Link> */}
+            </>
+        );
+    }
+
+}
