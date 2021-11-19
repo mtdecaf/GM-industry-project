0376ff6c46f97994bb719b2114bb6de78b36c159
Jacky Cao <jcvan0116@gmail.com>
2021-11-18 22:43:44 -0800

    app.get done

--- server/server.js
+++ server/server.js
@@ -9,6 +9,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+app.get("/", (req, res) => {
+    try{
+    const data = JSON.parse(fs.readFileSync("./data/data.json"));
+    // res.send(data);
+    res.status(200).json(data);
+    } catch(err){
+        res.status(404).send(err);
+    }
+});
 
 // gets the coordinates of the squre drawn by the user
 app.post("/", (req, res) => {
